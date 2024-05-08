f1abbfdd80e4b3804ebf27f7383ca776dfd73005
mwolft <166837201+mwolft@users.noreply.github.com>
2024-05-08 08:17:43 +0000

    mi generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["Tizón", "Mi padre", "Mi hermana", "Elena"];
+  let action = ["perdió", "no encontró", "olvidó", "rompió"];
+  let what = ["las bolsitas", "la correa", "las pastillas"];
+  let when = [
+    "antes de salir de paseo",
+    "cuando estaban en el pueblo",
+    "mientras estaban de paseo en el cementerio",
+    "durante el paseo de la tarde",
+    "mientras estaban jugando a la pelota"
+  ];
+  document.getElementById("excuse").innerHTML =
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)];
 };
